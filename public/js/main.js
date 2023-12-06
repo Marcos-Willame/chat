f207d1fc6efffcb1f80d06a07cb5f00c69c8b856
marcos-willame <marcos.willame.ti@gmail.com>
2023-12-05 22:54:22 -0300

    troca

--- public/js/main.js
+++ public/js/main.js
@@ -1,2 +1,2 @@
-const WS_URL = "ws://localhost:8080";
-//const WS_URL = "wss://roulette-chat-f12492d53689.herokuapp.com";
+//const WS_URL = "ws://localhost:8080";
+const WS_URL = "wss://roulette-chat-f12492d53689.herokuapp.com";
