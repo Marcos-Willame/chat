bdb0aad7b6fb3780e3bc484fd7e8c675ff082e35
marcos-willame <marcos.willame.ti@gmail.com>
2023-12-17 13:54:21 -0300

    atulização audio

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+/*
 //server.js
 
 const dotenv = require("dotenv");
@@ -58,7 +59,66 @@ app.use(express.static("public"));
 server.listen(PORT, () => {
   console.info(`Servidor Express rodando na porta ${PORT}`);
 });
+*/
 
+
+// server.js
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+const http = require("http");
+const express = require("express");
+
+const { WebSocketServer, WebSocket } = require("ws");
+
+const { handleChat } = require("./chat");
+const { handleRoulette } = require("./roulette");
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocketServer({ server });
+
+wss.on("connection", (ws) => {
+  console.info("Cliente conectado");
+  setupWebSocket(ws);
+});
+
+function setupWebSocket(ws) {
+  ws.on("error", console.error);
+
+  ws.on("message", (message) => {
+    if (Buffer.isBuffer(message)) {
+      wss.clients.forEach((client) => {
+        if (client !== ws && client.readyState === WebSocket.OPEN) {
+          client.send(message, { binary: true });
+        }
+      });
+    } else {
+      try {
+        const data = JSON.parse(message);
+
+        if (data.action === "chat") {
+          handleChat({ ws, data, server: wss });
+        } else if (data.action === "roulette") {
+          handleRoulette({ ws, data, server: wss });
+        }
+      } catch (error) {
+        console.error("Erro ao fazer parse da mensagem JSON:", error);
+      }
+    }
+  });
+
+  ws.on("close", () => console.info("Cliente desconectado"));
+}
+
+app.use(express.static("public"));
+
+server.listen(PORT, () => {
+  console.info(`Servidor Express rodando na porta ${PORT}`);
+});
 
 
 
