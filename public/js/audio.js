ff3403ff020d343376a6565526d145c00cadaedb
marcos-willame <marcos.willame.ti@gmail.com>
2023-12-19 10:57:57 -0300

    atulização audio

--- public/js/audio.js
+++ public/js/audio.js
@@ -1,94 +1,4 @@
 // audio.js
-
-const { setUsername, getUsername } = require('./log.mjs');
-
-const recordingButton = document.getElementById('recordingButton');
-let mediaRecorder;
-let ws;
-
-const createAudioElement = (audioBlob, sender) => {
-  const receivedBlob = new Blob([audioBlob], { type: 'audio/wav' });
-  const receivedAudioUrl = URL.createObjectURL(receivedBlob);
-
-  const audioPlayer = new Audio(receivedAudioUrl);
-  audioPlayer.controls = true;
-  audioPlayer.title = getUsername();
-
-  const messageContainer = document.createElement('div');
-  messageContainer.classList.add('message');
-  messageContainer.classList.add(sender === getUsername() ? 'sent' : 'received');
-  messageContainer.appendChild(audioPlayer);
-
-  chatMessages.appendChild(messageContainer);
-};
-
-const initWebSocket = () => {
-  ws = new WebSocket(WS_URL);
-
-  ws.onopen = () => {
-    console.log('WebSocket conectado.');
-    recordingButton.disabled = false;
-  };
-
-  ws.onmessage = (event) => {
-    if (event.data instanceof Blob && event.data.size > 0) {
-      createAudioElement(event.data, 'other');
-    }
-  };
-};
-
-const startRecording = async () => {
-  try {
-    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-
-    mediaRecorder = new MediaRecorder(stream);
-
-    mediaRecorder.ondataavailable = async (event) => {
-      if (event.data.size > 0) {
-        ws.send(event.data);
-        createAudioElement(event.data, 'self');
-      }
-    };
-
-    mediaRecorder.onstop = () => {
-      mediaRecorder.stream.getTracks().forEach((track) => {
-        track.stop();
-      });
-    };
-
-    mediaRecorder.start();
-  } catch (error) {
-    console.error('Erro ao acessar o microfone:', error);
-  }
-};
-
-const stopRecording = () => {
-  if (mediaRecorder && mediaRecorder.state === 'recording') {
-    mediaRecorder.stop();
-  }
-};
-
-recordingButton.addEventListener('mousedown', () => {
-  startRecording();
-});
-
-recordingButton.addEventListener('mouseup', () => {
-  stopRecording();
-});
-
-const setUsernameAndInitWebSocket = () => {
-  setUsername();
-  initWebSocket();
-};
-
-setUsernameAndInitWebSocket();
-
-
-
-
-
-/*
-// audio.js
 const recordingButton = document.getElementById('recordingButton');
 let mediaRecorder;
 let ws;
@@ -170,6 +80,6 @@ const setUsername = () => {
 
 setUsername();
 initWebSocket();
-*/
+
 
 
